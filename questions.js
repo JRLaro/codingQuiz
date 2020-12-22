4b71c6194d1e788ad716fd99ef268914761a0d21
J.Laro <joselarojr@gmail.com>
2020-12-22 13:42:15 -0500

    attempted to convert v-js to jquery

---
+++ questions.js
@@ -0,0 +1,39 @@
+///
+var questions = [
+    {
+        title: "In Season 1: What does Michael Scott pretend to fire Pam for?",
+        choices: ["Not answering the phones", "Stealing post-it notes", "Wearing her Glasses", "Not letting her hair down"],
+        answer: "Stealing post-it notes",
+    },
+    {
+        title: "What's Phyllis's maiden name?",
+        choices: ["Lapin", "Vougn", "Vance", "Lanpin"],
+        answer: "Lapin",
+    },
+    {
+        title: "What does Kevin suggest Dwight put in his gun holster?",
+        choices: ["A toy gun", "A cell phone", "A banana", "A Stapler"],
+        answer: "A banana",
+    },
+    {
+        title: "What kind of sandwich does Michael have a dream about when he's the head of Michael Scott Paper Company?",
+        choices: ["Peanut butter and tuna", "Tuna and skittles", "Mayo and black olives", "Black olives, peanut butter and tuna"],
+        answer: "Peanut butter and tuna",
+    },
+    {
+        title: "What radio station stickers are seen around the office?",
+        choices: ["Foggy 101", "Froggy 109", "Frogy 109", "Froggy 101"],
+        answer: "Froggy 101",
+    },
+    {
+        title: "What does Andy pose as during the lunch with the insurance salesman whom they suspect to be in the mob?",
+        choices: ["A police Officer", "A mechanic", "A mobster", "Theater scholar"],
+        answer: "A mechanic",
+    },
+    {
+        title: "How much did Michael's beloved plasma TV cost?",
+        choices: ["$200", "$100", "It was a gift", "$500"],
+        answer: "$200",
+    },
+];
+
